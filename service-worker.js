606c903659c69486c5433c55cff0aedb6b3c614a
VCET-saldo <happy_wietblaadje@hotmail.com>
2026-08-11 10:48:46 +0200

    Copy website files from vcet-calculator repo

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('vcet-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './vcet-logo.png',
+        './favicon.ico',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
